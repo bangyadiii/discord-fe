392ab837abfa8b5b2552277a10b7747a9e330c6e
EBay12Y <ekabayus12@gmail.com>
2024-09-24 11:06:24 +0700

    test deploy

--- src/app/api/friends/route.ts
+++ src/app/api/friends/route.ts
@@ -4,54 +4,56 @@ import { RequestStatus } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    try {
-        const session = await currentProfile();
-        if (!session) {
-            return NextResponse.json(
-                { message: "Unauthenticated" },
-                { status: 401 }
-            );
-        }
-        const friendList = await db.friend.findMany({
-            where: {
-                status: RequestStatus.ACCEPTED,
-                OR: [
-                    {
-                        senderId: session.id,
-                    },
-                    {
-                        receiverId: session.id,
-                    },
-                ],
-            },
-        });
-
-        const friends = await db.user.findMany({
-            where: {
-                AND: [
-                    {
-                        id: {
-                            in: friendList.map((friend) =>
-                                friend.senderId === session.id
-                                    ? friend.receiverId
-                                    : friend.senderId
-                            ),
-                        },
-                    },
-                    {
-                        NOT: {
-                            id: session.id,
-                        },
-                    },
-                ],
-            },
-        });
-        return NextResponse.json({ data: friends });
-    } catch (error) {
-        console.log(`Error: ${error}`);
-        NextResponse.json(
-            { message: "Something went wrong", error },
-            { status: 500 }
-        );
+  try {
+    const session = await currentProfile();
+    if (!session) {
+      return NextResponse.json({ message: "Unauthenticated" }, { status: 401 });
     }
+
+    const friendList = await db.friend.findMany({
+      where: {
+        status: RequestStatus.ACCEPTED,
+        OR: [
+          {
+            senderId: session.id,
+          },
+          {
+            receiverId: session.id,
+          },
+        ],
+      },
+    });
+
+    const friends = await db.user.findMany({
+      where: {
+        AND: [
+          {
+            id: {
+              in: friendList.map((friend) =>
+                friend.senderId === session.id
+                  ? friend.receiverId
+                  : friend.senderId
+              ),
+            },
+          },
+          {
+            NOT: {
+              id: session.id,
+            },
+          },
+        ],
+      },
+    });
+
+    return NextResponse.json({ data: friends });
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    return NextResponse.json(
+      {
+        message: "Something went wrong",
+        error: error instanceof Error ? error.message : error,
+      },
+      { status: 500 }
+    );
+  }
 }
